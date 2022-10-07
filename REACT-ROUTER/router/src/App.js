abadae4b21018cac13d7d571883db768ff0eab91
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-10-07 20:31:43 -0300

    Created component NavBar

--- REACT-ROUTER/router/src/App.js
+++ REACT-ROUTER/router/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
 
+// 1 - config react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
+//components
+import NavBar from "./components/NavBar";
+
 function App() {
 	return (
 		<div className="App">
 			<h1>React Router</h1>
+			<BrowserRouter>
+				<NavBar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
