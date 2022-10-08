de85262bc6880011ac3eaa1aa93efe69860aa978
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-10-07 22:26:54 -0300

    Added dynamics datas

--- REACT-ROUTER/router/src/App.js
+++ REACT-ROUTER/router/src/App.js
@@ -3,22 +3,27 @@ import "./App.css";
 // 1 - config react router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//components
+import NavBar from "./components/NavBar";
+
 //pages
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-
-//components
-import NavBar from "./components/NavBar";
+import Product from "./Pages/Product";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>React Router</h1>
+
+			{/*// 2- links com react router*/}
 			<BrowserRouter>
 				<NavBar />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
+					{/* 4 - Rota dinâmica */}
+					<Route path="/products/:id" element={<Product />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
--- REACT-ROUTER/router/src/Pages/Home.js
+++ REACT-ROUTER/router/src/Pages/Home.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Home.css";
 
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
 const Home = () => {
-	return <div>Home</div>;
+	// 3 - carregamento de dados
+	const url = "http://localhost:3000/products";
+
+	const { data: items, loading, error } = useFetch(url);
+
+	return (
+		<div>
+			<h2>Produtos</h2>
+			{error && <p>{error}</p>}
+			<ul className="products">
+				{items &&
+					items.map((item) => (
+						<li key={item.id}>
+							<h3>{item.name} </h3>
+							<p>R$: {item.price} </p>
+							{/* 4 - Rota dinâmica */}
+							<Link to={`/products/${item.id}`}>Detalhes</Link>
+						</li>
+					))}
+			</ul>
+		</div>
+	);
 };
 
 export default Home;
